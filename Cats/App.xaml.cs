65baccfa62c0470c6ae95cc124d8e7f6fb60f0dc
anderson.silva <andermalk16@gmail.com>
2017-03-03 11:09:30 -0300

    prmieiro commit lab maratona xamarin

---
+++ Cats/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace Cats
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			// The root page of your application
+			var content = new Cats.Views.CatsPage();
+
+			MainPage = new NavigationPage(content);
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Cats/ViewModels/CatsViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cats.ViewModels
+{
+	public class CatsViewModel : INotifyPropertyChanged
+	{
+
+		public CatsViewModel()
+		{
+			Cats = new ObservableCollection<Cats.Models.Cat>();
+			GetCatsCommand = new Command(async () => await GetCats(), () => !IsBusy);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName]string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+		private bool Busy;
+		public bool IsBusy
+		{
+			get
+			{
+				return Busy;
+			}
+			set
+			{
+				Busy = value;
+				OnPropertyChanged();
+				GetCatsCommand.ChangeCanExecute();
+			}
+		}
+
+		public Command GetCatsCommand { get; set; }
+
+
+		public ObservableCollection<Cats.Models.Cat> Cats { get; set; }
+
+		async Task GetCats()
+		{
+			if (!IsBusy)
+			{
+				Exception Error = null;
+				try
+				{
+					IsBusy = true;
+					var Repository = new Cats.Models.Repository();
+					var Items = await Repository.GetCats();
+					Cats.Clear();
+					foreach (var Cat in Items)
+					{
+						Cats.Add(Cat);
+					}
+				}
+				catch (Exception ex)
+				{
+					Error = ex;
+				}
+				finally
+				{
+					IsBusy = false;
+					if (Error != null)
+					{
+						await Xamarin.Forms.Application.Current.MainPage.DisplayAlert(
+						"Error!", Error.Message, "OK");
+					}
+				}
+			}
+			return;
+		}
+	}
+}
